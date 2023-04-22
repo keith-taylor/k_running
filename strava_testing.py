cef6b84357f313424c994ba0a474a887f2206c2c
Keith Taylor <k-taylor@outlook.com>
2023-04-22 17:13:52 +0100

    Got access token working. Added pace info.

--- strava_testing.py
+++ strava_testing.py
@@ -9,23 +9,27 @@ activites_url = "https://www.strava.com/api/v3/athlete/activities"
 payload = {
     'client_id': "106016",
     'client_secret': 'dd43cbabc090ed167750e44caaf17ab309aa7da5',
-    'refresh_token': '0069492457c0e43f120495a203b954f6bb69669f',
+    'refresh_token': 'ab22c653921ac96d02b9b5a22386e9300cadbb0a',
     'grant_type': "refresh_token",
     'f': 'json'
 }
 
-print("Requesting Token...\n")
+print("Requesting Access Token...\n")
 res = requests.post(auth_url, data=payload, verify=False)
-#access_token = res.json()['access_token']
-access_token = '256b159c464ef2cc0a46f1b2cb1a87b2dc3d4621'
-print("Access Token = {}\n".format(access_token))
+#print(res.json())
+access_token = res.json()['access_token']
 
-header = {'Authorization': 'Bearer ' + access_token, 'read_permission': "read_all"}
+#print(f"Access Token: {access_token}\n")
+
+header = {'Authorization': 'Bearer ' + access_token,'read_permission': "read_all"}
 param = {'per_page': 200, 'page': 1}
 my_dataset = requests.get(activites_url, headers=header, params=param).json()
 
 print("Your last ten runs are: ")
-for i in range (1,11):
+
+for i in range (0,10):
     run_name = my_dataset[i]["name"].strip()
     run_length = round((my_dataset[i]["distance"]/1000), 1)
-    print(f'{i}\t {run_name} - {run_length} km.')
+    run_pace = round((my_dataset[i]["moving_time"] / 60 / run_length), 2)
+    print(f'{i+1}\t {run_name}: {run_length} km at an average {run_pace} min/km pace.')
+
