ff6ee72154a91e35f6cd7b13b113fa49b1bfa08c
Keith Taylor <k-taylor@outlook.com>
2023-04-23 22:10:12 +0100

    .

--- strava_data_calls.py
+++ strava_data_calls.py
@@ -3,8 +3,6 @@ import urllib3
 import json # for debugging purposes (print pretty json)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-
 def get_strava_activites(user_access_token, f_activities_per_page, f_num_pages, f_url):
     my_header = {'Authorization': 'Bearer ' + user_access_token,'read_permission': "read_all"}
     my_params = {'per_page': f_activities_per_page, 'page': f_num_pages}
