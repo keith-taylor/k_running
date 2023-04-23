3f906d801bfc3d8c137e3191aded3b46ce718083
keith-taylor <k-taylor@outlook.com>
2023-04-23 21:56:42 +0100

    added a new module for functions that process and print Strava data.Also included improvments to check len() of returned activites in 'print_last_ten_activities' and word output accordingly.

---
+++ data_processes.py
@@ -0,0 +1,17 @@
+import inflect 
+p = inflect.engine()
+
+
+def print_last_ten_activities(f_dataset):
+    num_activities_in_words = p.number_to_words(min(10, len(f_dataset)))
+    if len(f_dataset) == 1:
+        print(f"Your only activity so far is: ")
+    else:
+        print(f"Your last {num_activities_in_words} activities were: ")
+    for i in range (0, min(10, len(f_dataset))):
+        activity_name = f_dataset[i]["name"].strip()
+        activity_distance = round((f_dataset[i]["distance"]/1000), 1)
+        activity_pace = round((f_dataset[i]["moving_time"] / 60 / activity_distance), 2)
+        sport_type = (f_dataset[i]["sport_type"]).lower()
+        print(f'{i+1}\t \'{activity_name}\': a {activity_distance} km {sport_type} with an average {activity_pace} min/km pace.')
+    
--- k_running.py
+++ k_running.py
@@ -1,12 +1,15 @@
 import requests
+import json
 from strava_tokens import get_strava_access_token
 from strava_data_calls import get_strava_activites 
+from data_processes import print_last_ten_activities
+import pprint
 
 activities_url = "https://www.strava.com/api/v3/athlete/activities"
 auth_url = "https://www.strava.com/oauth/token"
 athletes_url = "https://www.strava.com/api/v3/athlete/athlete"
 
-activities_per_page = 200
+activities_per_page = 9
 num_pages = 1
 
 user_info = {
@@ -20,11 +23,8 @@ access_token = get_strava_access_token(user_info, auth_url)
 activities_dataset = get_strava_activites(access_token, activities_per_page, num_pages,
                                           activities_url) 
 
-print("Your last ten runs are: ")
+print_last_ten_activities(activities_dataset)
 
-for i in range (0,10):
-    run_name = activities_dataset[i]["name"].strip()
-    run_length = round((activities_dataset[i]["distance"]/1000), 1)
-    run_pace = round((activities_dataset[i]["moving_time"] / 60 / run_length), 2)
-    print(f'{i+1}\t {run_name}: {run_length} km at an average {run_pace} min/km pace.')
-
+#activities_dataset_dic = activities_dataset[0]
+#pretty_activities_dataset = json.dumps(activities_dataset_dic, indent = 4)
+#print(pretty_activities_dataset)
