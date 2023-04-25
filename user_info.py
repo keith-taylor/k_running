05cf5b95ea11a3919eb28e43bdec21b86e68f9f5
Keith Taylor <k-taylor@outlook.com>
2023-04-25 19:01:26 +0100

    updated requirments.txt from pip and added new user secrets to user_info file

--- user_info.py
+++ user_info.py
@@ -2,7 +2,7 @@ keith_taylor = {
     'user_name': 'keith-taylor',
     'vanity_url': 'https://www.strava.com/athletes/keith-taylor',
     'client_id': "106016",
-    'client_secret': 'dd43cbabc090ed167750e44caaf17ab309aa7da5',
+    'client_secret': '3d7f36317130074d05378ea1f1ac6ba889915a0b',
     'refresh_token': 'ab22c653921ac96d02b9b5a22386e9300cadbb0a',
 }
 
@@ -10,6 +10,6 @@ keith_test = {
     'user_name': 'keith-taylor',
     'vanity_url': 'https://www.strava.com/athletes/keith_api',
     'client_id': "106247",
-    'client_secret': '570c7611ecb7f63666458b789d743bed06683a07',
+    'client_secret': 'c85f85bee35a7b5b379776da7117924e7240375e',
     'refresh_token': 'b4181e935807b17bb8690f42a264fbcdceb87549', 
 }
