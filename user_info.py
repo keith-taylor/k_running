68369f031e2d15599ad78b0331bc4bbc65fad13e
Keith Taylor <k-taylor@outlook.com>
2023-04-25 18:48:57 +0100

    adding user_info.py

---
+++ user_info.py
@@ -0,0 +1,15 @@
+keith_taylor = {
+    'user_name': 'keith-taylor',
+    'vanity_url': 'https://www.strava.com/athletes/keith-taylor',
+    'client_id': "106016",
+    'client_secret': 'dd43cbabc090ed167750e44caaf17ab309aa7da5',
+    'refresh_token': 'ab22c653921ac96d02b9b5a22386e9300cadbb0a',
+}
+
+keith_test = {
+    'user_name': 'keith-taylor',
+    'vanity_url': 'https://www.strava.com/athletes/keith_api',
+    'client_id': "106247",
+    'client_secret': '570c7611ecb7f63666458b789d743bed06683a07',
+    'refresh_token': 'b4181e935807b17bb8690f42a264fbcdceb87549', 
+}
